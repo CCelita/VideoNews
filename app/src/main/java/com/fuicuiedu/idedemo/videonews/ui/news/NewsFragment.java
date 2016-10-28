59465baf102fd03ecb94544c8b5fb4356cd169fe
wxcican <wxcican@qq.com>
2016-10-28 12:52:18 +0800

    app模块初搭建

---
+++ app/src/main/java/com/fuicuiedu/idedemo/videonews/ui/news/NewsFragment.java
@@ -0,0 +1,15 @@
+package com.fuicuiedu.idedemo.videonews.ui.news;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2016/10/28 0028.
+ */
+
+public class NewsFragment extends Fragment {
+}
